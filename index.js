99228a4e4e9696a32d84ee76b5c3ee5a8b09b4c3
Edward <cololacalempira5@gmail.com>
2026-07-19 16:46:47 -0600

    Update to sin-mongo version: remove mongoose dependency and models

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require('cors');
 const path = require('path');
 
 const loadRoutes = require('./utils/loadRoutes');
-const connectDB = require('./utils/db');
 const { seedAdmin } = require('./utils/seedAdmin');
 const { notFound, errorHandler } = require('./middleware/errorHandler');
 
@@ -49,19 +48,8 @@ app.use(errorHandler);
 
 const PORT = process.env.PORT || 3000;
 
-async function start() {
+seedAdmin().catch(err => console.error('Error al crear cuenta admin:', err));
 
-    await connectDB();
-
-    await seedAdmin().catch(err => console.error('Error al crear cuenta admin:', err));
-
-    app.listen(PORT, () => {
-        console.log(`Servidor corriendo en el puerto ${PORT}`);
-    });
-
-}
-
-start().catch(err => {
-    console.error('Error al iniciar el servidor:', err);
-    process.exit(1);
+app.listen(PORT, () => {
+    console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
