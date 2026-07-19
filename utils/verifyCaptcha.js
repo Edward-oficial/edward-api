53fb353c5481a6347f7ca63f54947c1dccdead59
Edward <cololacalempira5@gmail.com>
2026-07-19 00:28:50 -0600

    Create verifyCaptcha.js

---
+++ utils/verifyCaptcha.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+
+// Secret Key de hCaptcha (dashboard.hcaptcha.com)
+const HCAPTCHA_SECRET = 'ES_0cf00826876f4e40abd1c276f295b507';
+
+async function verifyCaptcha(token) {
+    if (!token) return false;
+
+    try {
+        const { data } = await axios.post(
+            'https://hcaptcha.com/siteverify',
+            new URLSearchParams({
+                secret: HCAPTCHA_SECRET,
+                response: token
+            })
+        );
+
+        return data.success === true;
+    } catch (err) {
+        return false;
+    }
+}
+
+module.exports = verifyCaptcha;
+  
